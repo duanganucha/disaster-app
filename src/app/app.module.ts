42e5e73c436177bbd223b480b73979daf4e14686
duanganucha <duanganucha@hotmail.com>
2019-03-27 20:45:55 +0700

    tab

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-CfvT1RFiJv-f6p0QU2e5fIoFQsrVm84",
+  authDomain: "disaster-e4065.firebaseapp.com",
+  databaseURL: "https://disaster-e4065.firebaseio.com",
+  projectId: "disaster-e4065",
+  storageBucket: "disaster-e4065.appspot.com",
+  messagingSenderId: "941580326823"
+};
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AngularFireDatabase,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
